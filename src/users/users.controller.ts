b0d9455ee280e4dd7819c4f5d71c30c79472c728
Caio <dandinzin@gmail.com>
2026-04-04 20:02:54 -0300

    feat: add swagger in user controller

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,18 +1,25 @@
-import { Body, Controller, Put, UseGuards, Request } from '@nestjs/common'
-import { AuthGuard } from '@nestjs/passport'
-import { UsersService } from './users.service'
+import { Body, Controller, Put, UseGuards, Request } from '@nestjs/common';
+import { AuthGuard } from '@nestjs/passport';
+import {
+  ApiBearerAuth,
+  ApiOperation,
+  ApiResponse,
+  ApiTags,
+} from '@nestjs/swagger';
+import { UsersService } from './users.service';
+import { UpdateUserDto } from './dto/update-user.dto';
 
+@ApiTags('Users')
 @Controller('users')
 export class UserController {
-  
   constructor(private readonly userService: UsersService) {}
 
+  @ApiBearerAuth()
+  @ApiOperation({ summary: 'Atualizar perfil do usuário logado' })
+  @ApiResponse({ status: 200, description: 'Usuário atualizado com sucesso' })
   @UseGuards(AuthGuard('jwt'))
   @Put('me')
-  async updateMe(
-    @Request() req,
-    @Body() body: { name: string }
-  ) {
-    return this.userService.updateUser(req.user.sub, body)
+  async updateMe(@Request() req, @Body() body: UpdateUserDto) {
+    return this.userService.updateUser(req.user.sub, body);
   }
 }
