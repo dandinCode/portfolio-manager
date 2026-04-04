d4e23620c2347f34a64b18c997e1a1b94a2d7914
Caio <dandinzin@gmail.com>
2026-04-04 19:13:04 -0300

    feat: criando rota de update user

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,4 +1,18 @@
-import { Controller, Get, Req, UseGuards } from '@nestjs/common';
+import { Body, Controller, Put, UseGuards, Request } from '@nestjs/common'
+import { AuthGuard } from '@nestjs/passport'
+import { UsersService } from './users.service'
 
 @Controller('users')
-export class UsersController {}
+export class UserController {
+  
+  constructor(private readonly userService: UsersService) {}
+
+  @UseGuards(AuthGuard('jwt'))
+  @Put('me')
+  async updateMe(
+    @Request() req,
+    @Body() body: { name: string }
+  ) {
+    return this.userService.updateUser(req.user.sub, body)
+  }
+}
