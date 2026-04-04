cb30dfbd50a0d8126de99ea16d4bb836b39eaf7a
Caio <dandinzin@gmail.com>
2026-04-04 20:02:30 -0300

    feat: add swagger in portfolio controller

--- src/portfolio/portfolio.controller.ts
+++ src/portfolio/portfolio.controller.ts
@@ -11,23 +11,41 @@ import {
 import { PortfolioService } from './portfolio.service';
 import { CreatePortfolioDto } from './dto/create-portfolio.dto';
 import { AuthGuard } from '@nestjs/passport';
+import {
+  ApiBearerAuth,
+  ApiOperation,
+  ApiResponse,
+  ApiTags,
+  ApiParam,
+} from '@nestjs/swagger';
 
+@ApiTags('Portfolio')
 @Controller('portfolio')
 export class PortfolioController {
   constructor(private readonly portfolioService: PortfolioService) {}
 
+  @ApiBearerAuth()
+  @ApiOperation({ summary: 'Criar novo portfólio' })
+  @ApiResponse({ status: 201, description: 'Portfólio criado com sucesso' })
   @UseGuards(AuthGuard('jwt'))
   @Post()
   async create(@Req() req: any, @Body() dto: CreatePortfolioDto) {
     return this.portfolioService.create(req.user.sub, dto);
   }
 
+  @ApiBearerAuth()
+  @ApiOperation({ summary: 'Listar portfólios do usuário' })
+  @ApiResponse({ status: 200, description: 'Lista de portfólios' })
   @UseGuards(AuthGuard('jwt'))
   @Get()
   async findMyPortfolios(@Req() req: any) {
     return this.portfolioService.findAllByUser(req.user.sub);
   }
 
+  @ApiBearerAuth()
+  @ApiOperation({ summary: 'Excluir um portfólio' })
+  @ApiParam({ name: 'id', example: 1 })
+  @ApiResponse({ status: 200, description: 'Portfólio excluído com sucesso' })
   @UseGuards(AuthGuard('jwt'))
   @Delete(':id')
   async delete(@Param('id') id: string, @Req() req) {
