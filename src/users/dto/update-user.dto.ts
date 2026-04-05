d6f70e962c91c8512ee927f85fef52894e4310e0
Caio <dandinzin@gmail.com>
2026-04-05 09:07:56 -0300

    feat: corrigindo to de update user

--- src/users/dto/update-user.dto.ts
+++ src/users/dto/update-user.dto.ts
@@ -1,9 +1,12 @@
 import { ApiProperty } from '@nestjs/swagger';
+import { IsNotEmpty, IsString } from 'class-validator';
 
 export class UpdateUserDto {
   @ApiProperty({
     example: 'João Silva',
     description: 'Nome do usuário',
   })
+  @IsString()
+  @IsNotEmpty()
   name: string;
 }
