2c779f0cb000d818ad30ee6fde1767ca0642176d
Caio <dandinzin@gmail.com>
2026-04-24 21:57:33 -0300

    fix: validando symbol antes de registrar

--- src/stocks/dto/create-stock-symbol.dto.ts
+++ src/stocks/dto/create-stock-symbol.dto.ts
@@ -1,8 +1,13 @@
 import { ApiProperty } from '@nestjs/swagger';
-import { IsString, IsInt, IsOptional } from 'class-validator';
+import { IsString, Matches, Length } from 'class-validator';
 
 export class CreateStockSymbolDto {
   @ApiProperty({ example: 'PETR4', description: 'Símbolo da ação' })
   @IsString()
+  @Length(4, 6, { message: 'O símbolo deve ter entre 4 e 6 caracteres.' })
+  @Matches(/^[A-Z]{4}\d{1,2}$/, {
+    message:
+      'Formato inválido. Use padrão como PETR4 (4 letras maiúsculas + 1 ou 2 números).',
+  })
   symbol: string;
 }
