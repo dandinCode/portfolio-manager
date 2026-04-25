6509f05b2502110f2d93500506f049b81ad6ec85
Caio <dandinzin@gmail.com>
2026-04-24 21:42:45 -0300

    feat: removendo permissao pra localhost no cors

--- src/main.ts
+++ src/main.ts
@@ -7,8 +7,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
     origin: [
-      process.env.FRONTEND_URL,
-      'http://localhost:5173',
+      process.env.FRONTEND_URL
     ],
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
     credentials: true,
