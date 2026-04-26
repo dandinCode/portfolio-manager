faae28ce858cf3ebea58740a44cc59699991610f
Caio <dandinzin@gmail.com>
2026-04-25 22:06:27 -0300

    feat: instalando cookie-parser

--- src/main.ts
+++ src/main.ts
@@ -2,9 +2,13 @@ import { NestFactory } from '@nestjs/core';
 import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import * as cookieParser from 'cookie-parser';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  
+  app.use(cookieParser());
+  
   app.enableCors({
     origin: [
       process.env.FRONTEND_URL
